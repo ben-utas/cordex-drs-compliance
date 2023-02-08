d03f9c9520e21016e10ffc1f2026d3a59e62b447
Ben Mildren <benjamin.mildren@utas.edu.au>
2023-02-08 14:56:21 +1100

    Split files that had combined types of output

--- compliance.py
+++ compliance.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 
 from cdo import * # Provides functions for the manipulation of climate data NetCDF files.
-import nco
+from nco import Nco # Provides functions for the manipulation of general NetCDF files.
+from nco.custom import Atted
 from pathlib import Path # Provides functions for manipulating files.
 
 cdo = Cdo()
+nco = Nco()
 
 # Search for all files with a .nc extension in the current directory.
 path = Path('.')
@@ -13,5 +15,8 @@ nc_files = list(path.glob('*.nc'))
 # Find files with name containing the date range 2000-2009, and split this file into a historical and projected file with historical file having metadata updated.
 for nc_file in nc_files:
     if "2000-2009" in nc_file.name:
-        nc_experiment = nc_file.name.split("_")[0]
-        cdo.selyear("2000/2005", input=nc_file, output="historical.nc")
+        nc_historical = nc_file.name.split("_")[0] + "2000-2005.nc"
+        nc_experiment = nc_file.name.split("_")[0] + "2006-2009.nc"
+        cdo.selyear("2000/2005", input=nc_file, output=nc_historical)
+        cdo.selyear("2006/2009", input=nc_file, output=nc_experiment)
+        nco.ncrename
