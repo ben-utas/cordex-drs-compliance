38b6c99b42c687d9a1728e2e77122e78c5a78d90
Ben Mildren <benjamin.mildren@utas.edu.au>
2023-02-09 12:31:00 +1100

    Fixed attribute modification.

--- README.md
+++ README.md
@@ -3,3 +3,17 @@
 This repo stores scripts built in Python 3 and BASH to manipulate NetCDF climate data files to prepare them for storarge following the CORDEX data reference syntax, as specified [here](http://is-enes-data.github.io/cordex_archive_specifications.pdf).
 
 In its current state these scripts are built for a specific case of files retrieved by the Climate Futures group at UTAS from CSIRO that needed some manipulations. As a result, they will not work with other filesystems. However, if this program is successful it may be updated in future to be a general use script that extracts data from a NetCDF file that is CORDEX compliant and correctly files it in a specified directory.
+
+## Requirements
+
+Due to it's requirements, this script is best suited towards linux based systems.
+
+### Python Packages
+    - [cdo](https://pypi.org/project/cdo/)
+    - [nco](https://pypi.org/project/nco/)
+    - [numpy](https://pypi.org/project/numpy/)
+
+### Linux Packages
+    - Python 3.6 or above
+    - [cdo](https://packages.debian.org/sid/cdo)
+    - [nco](https://nco.sourceforge.net/nco.html)
--- compliance.py
+++ compliance.py
@@ -35,6 +35,7 @@ invariant_variables = [
 
 cdo = Cdo()
 nco = Nco()
+nco.debug = True
 
 # Search for all files with a .nc extension in the current directory.
 path = Path('.')
@@ -42,41 +43,44 @@ nc_files = list(path.glob('*.nc'))
 
 # Options for renaming historical based output variable files.
 opt_historical = [
-    Atted("m", "experiment_id", "global attributes", "historical"),
-    Atted("m", "experiment", "global attributes", "historical"),
-    Atted("m", "driving_experiment", "global attributes", "historical"),
-    Atted("m", "driving_experiment_name", "global attributes", "historical"),
-    Atted("m", "domain", "global attributes", "GLB-50i"),
-    Atted("m", "comment", "global attributes", "GLB-50i"),
-    Atted("m", "frequency", "global attributes", "day")
+    ["-h"],
+    ["-a" " \'experiment_id\',global,o,c,\'historical\'"],
+    ["-a" " \'experiment\',global,o,c,\'historical\'"],
+    ["-a" " \'driving_experiment\',global,o,c,\'historical\'"],
+    ["-a" " \'driving_experiment_name\',global,o,c,\'historical\'"],
+    ["-a" " \'domain\',global,o,c,\'GLB-50i\'"],
+    ["-a" " \'comment\',global,o,c,\'GLB-50i\'"],
+    ["-a" " \'frequency\',global,o,c,\'day\'"]
 ]
 
 # Options for renaming experimental based output variable files.
 opt_experimental = [
-    Atted("m", "domain", "global attributes", "GLB-50i"),
-    Atted("m", "comment", "global attributes", "GLB-50i"),
-    Atted("m", "frequency", "global attributes", "day")
+    ["-h"],
+    ["-a" " \'domain\',global,o,c,\'GLB-50i\'"],
+    ["-a" " \'comment\',global,o,c,\'GLB-50i\'"],
+    ["-a" " \'frequency\',global,o,c,\'day\'"]
 ]
-
 
 for nc_file in nc_files:
     # Find files with name containing the date range 2000-2009, and split this file into a historical and projected file with historical file having metadata updated.
     if "2000-2009" in nc_file.name:
         nc_historical = nc_file.name.split(".")[0] + ".2000-2005.nc"
         nc_experiment = nc_file.name.split(".")[0] + ".2006-2009.nc"
-        cdo.selyear("2000/2005", input=nc_file, output=nc_historical)
-        cdo.selyear("2006/2009", input=nc_file, output=nc_experiment)
-        nco.ncatted(opt_historical, input=nc_historical)
-        nco.ncatted(opt_experimental, input=nc_experiment)
+        cdo.selyear("2000/2005", input=nc_file.name, output=nc_historical)
+        cdo.selyear("2006/2009", input=nc_file.name, output=nc_experiment)
+        nco.ncatted(input=nc_historical, options = opt_historical, output=nc_historical)
+        nco.ncatted(input=nc_experiment, options = opt_experimental, output=nc_experiment)
     # Fix metadata for remaining files.
-    if int(nc_file.name.split(".")[1].split("-")[0]) < 2000:
-        nco.ncatted(opt_historical, input=nc_file)
-    if int(nc_file.name.split(".")[1].split("-")[0]) > 2009:
-        nco.ncatted(opt_experimental, input=nc_file)
+    if int(nc_file.name.split(".")[1].split("-")[0]) <= 2005:
+        print("hist")
+        nco.ncatted(input=nc_file.name, options = opt_historical, output=nc_file.name)
+    if int(nc_file.name.split(".")[1].split("-")[0]) >= 2006:
+        print("exp")
+        nco.ncatted(input=nc_file.name, options=opt_experimental, output=nc_file.name)
     
     # Rename and relocate files according to DRS standard.
-    nc_headers = nco.ncdump(nc_file, h=True)
+    nc_headers = nco.ncdump(nc_file.name, h=True)
     print(nc_headers)
-    product = nco.ncdump(nc_file, h=True, v=True).split("product:")[1].split("")
+    product = nco.ncdump(nc_file.name, h=True, v=True).split("product:")[1].split("")
     print(product)
     
