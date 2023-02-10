0f407f6cf6b87d77ec91e6e89d68e32427cdbc6a
Ben Mildren <benjamin.mildren@utas.edu.au>
2023-02-10 15:38:33 +1100

    Replace NCO library and introduce relocation data

--- compliance.py
+++ compliance.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python3
 
 import argparse # Provides functions for parsing command line arguments.
+import subprocess # Provides functions for running shell commands.
 from pathlib import Path # Provides functions for manipulating files.
 
 from cdo import * # Provides functions for the manipulation of climate data NetCDF files.
-from nco import Nco # Provides functions for the manipulation of general NetCDF files.
-from nco.custom import Atted # Provides functions for the manipulation of NetCDF attributes.
 
 parser = argparse.ArgumentParser(
     prog="CORDEX Compliance Enforcer and Path Builder",
@@ -34,32 +33,48 @@ invariant_variables = [
 ]
 
 cdo = Cdo()
-nco = Nco()
-nco.debug = True
 
 # Search for all files with a .nc extension in the current directory.
 path = Path('.')
 nc_files = list(path.glob('*.nc'))
 
-# Options for renaming historical based output variable files.
-opt_historical = [
-    ["-h"],
-    ["-a" " \'experiment_id\',global,o,c,\'historical\'"],
-    ["-a" " \'experiment\',global,o,c,\'historical\'"],
-    ["-a" " \'driving_experiment\',global,o,c,\'historical\'"],
-    ["-a" " \'driving_experiment_name\',global,o,c,\'historical\'"],
-    ["-a" " \'domain\',global,o,c,\'GLB-50i\'"],
-    ["-a" " \'comment\',global,o,c,\'GLB-50i\'"],
-    ["-a" " \'frequency\',global,o,c,\'day\'"]
-]
+# Command for renaming historical based output variable files.
+opt_historical = (
+    "ncatted -O -h " +
+    "-a experiment_id,global,o,c,historical " +
+    "-a experiment,global,o,c,historical " +
+    "-a driving_experiment,global,o,c,historical " +
+    "-a driving_experiment_name,global,o,c,historical " +
+    "-a domain,global,o,c,GLB-50i " +
+    "-a comment,global,o,c,GLB-50i " +
+    "-a frequency,global,o,c,day "
+)
 
-# Options for renaming experimental based output variable files.
-opt_experimental = [
-    ["-h"],
-    ["-a" " \'domain\',global,o,c,\'GLB-50i\'"],
-    ["-a" " \'comment\',global,o,c,\'GLB-50i\'"],
-    ["-a" " \'frequency\',global,o,c,\'day\'"]
-]
+# Command for renaming experimental based output variable files.
+opt_experimental = (
+    "ncatted -O -h " +
+    "-a domain,global,o,c,GLB-50i " +
+    "-a comment,global,o,c,GLB-50i " +
+    "-a frequency,global,o,c,day "
+)
+
+def relocate(nc_file: Path):
+    """Rename and relocate files according to DRS standard.
+
+    Args:
+        nc_file (Path): Path to NetCDF file to be renamed and relocated.
+    """
+    nc_headers = subprocess.run("ncdump -h " + nc_file.name, stdout=subprocess.PIPE, shell=True)
+    nc_headers = nc_headers.stdout.decode("utf-8").split('\n')
+    variable_name = nc_file.name.split("_")[0]
+    # Find the string in nc_headers that contains each variable.
+    for s in nc_headers:
+        if "product" in s:
+            product = s.split("\"")[1]
+        if "domain" in s:
+            domain = s.split("\"")[1]
+        if "institution" in s:
+            institution = s.split("\"")[1]
 
 for nc_file in nc_files:
     # Find files with name containing the date range 2000-2009, and split this file into a historical and projected file with historical file having metadata updated.
@@ -68,19 +83,17 @@ for nc_file in nc_files:
         nc_experiment = nc_file.name.split(".")[0] + ".2006-2009.nc"
         cdo.selyear("2000/2005", input=nc_file.name, output=nc_historical)
         cdo.selyear("2006/2009", input=nc_file.name, output=nc_experiment)
-        nco.ncatted(input=nc_historical, options = opt_historical, output=nc_historical)
-        nco.ncatted(input=nc_experiment, options = opt_experimental, output=nc_experiment)
+        subprocess.run(opt_historical + nc_historical, shell=True)
+        subprocess.run(opt_experimental + nc_experiment, shell=True)
+        relocate(path.glob(nc_historical)[0])
+        relocate(path.glob(nc_experiment)[0])
     # Fix metadata for remaining files.
     if int(nc_file.name.split(".")[1].split("-")[0]) <= 2005:
-        print("hist")
-        nco.ncatted(input=nc_file.name, options = opt_historical, output=nc_file.name)
+        print(nc_file.name)
+        subprocess.run(opt_historical + nc_file.name, shell=True)
+        relocate(nc_file)
     if int(nc_file.name.split(".")[1].split("-")[0]) >= 2006:
-        print("exp")
-        nco.ncatted(input=nc_file.name, options=opt_experimental, output=nc_file.name)
-    
-    # Rename and relocate files according to DRS standard.
-    nc_headers = nco.ncdump(nc_file.name, h=True)
-    print(nc_headers)
-    product = nco.ncdump(nc_file.name, h=True, v=True).split("product:")[1].split("")
-    print(product)
+        print(nc_file.name)
+        subprocess.run(opt_experimental + nc_file.name, shell=True)
+        relocate(nc_file)
     
